c1a30fd7c9783c4efc71bbfa745d7c1e242629cc
Vagrant Default User <vagrant@jessie.raw>
2016-07-27 22:03:38 +0000

    Chef project

---
+++ chef_is_fun/holberton-wrapper/metadata.rb
@@ -0,0 +1,13 @@
+name             'holberton-wrapper'
+maintainer       'Josh Parkim'
+maintainer_email '14@holbertonschool.com'
+license          'All rights reserved'
+description      'Installs/Configures holberton-wrapper'
+long_description 'Installs/Configures holberton-wrapper'
+version          '0.1.0'
+
+depends 'ntp', '~> 2.0.0'
+depends 'openssh', '~> 2.0.0'
+depends 'apt', '~> 4.0.1'
+depends 'firewall', '~> 2.5.2'
+depends 'user', '~> 0.4.2'
---
+++ chef_is_fun/holberton-wrapper/recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: holberton-wrapper
+# Recipe:: default
+#
+# Copyright (C) 2016 Josh Parkin
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Add the dotdeb repo
+# Add the dotdeb repo
+include_recipe 'openssh'
+
+apt_repository 'dotdeb' do
+  uri 'http://packages.dotdeb.org'
+  components ['main']
+end
+
+# Install net-tools
+package 'net-tools'
+
+directory '/var/run/sshd' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+begin
+  r = resources(service: 'ssh')
+  if 'debian' == node['platform']
+    r.provider = Chef::Provider::Service::Debian
+  end
+end
+
+# User accounts
+user_account 'holberton' do
+  ssh_keys 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDRgomCmh+LcBwZOgmQZw3SvLFDBD0LGZN6ZoZnP70JvXMeEFiF9CtazJZObh6+PSwVkwrH5t3jznJcpm+vLDlmfy51WyPMj+vtTSVRfAacdv3N4D0/NiDUwwESvIQQgwQparyA/xkcbApRqa9ym7Le+0GQqRXFz/nt/HFijzbBDwfVNsc2aurfwLClJdzDrm6BZO4QAtE7OaMM/1yMOri6MahsOgl71OZ5dgUmVCUvJUZvGTVMnhgyFsYQ/aI5T0qQgQZoA0ao+nwqbKEjlAUJ/G5TqoczOXqWznYtfiHcdFCcBKdU9JLMZFNaFueHlCwrw4uEpw9v3ISKXhCJenDqNQYCIDDa/3nkvSKiqAGm189vDQOr8jt3rSCFZ7ceIW3ot+BV6UBFoKaF4z7Ve3W1QH8cnvUlL10G4HewHxVmeT762sWoRZcVQNygslCveii6BP8mPoayt8Gg1SDoFZBc90EwmOl/xvd/QB3FBd9ZkwA1ddATBiCk4LF6V2mo/f8l5Tb6gdR/2Vl0BUJhHyha26zdKpFpJr5ygqtuimH2OIZ4+W78RKH/0+/7OkKPl4KBbpyfD8yf+VU/fsKDXfjC5E+5V4+4Iu1DWLUiTc4xv+6/dxSTG3i4VcJ6DrgEk+TYRnCgKn7XKI/6maPPvtv/+olCzX4zQ/ehGIqO8N1IDw=='
+  comment 'Holberton School'
+end
+
+#Install and configure nginx
+package 'nginx'
+
+service 'nginx' do
+  action [:enable, :start]
+  provider Chef::Provider::Service::Debian
+end
